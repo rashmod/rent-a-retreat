8cdfff606a2893d2d16e565e1296946a217e5306
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-15 23:55:07 +0530

    frontend setup with typescript vite/react tailwindCSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='h-screen bg-sky-800 flex justify-center items-center'>
+			<h1 className='text-9xl font-bold text-white'>Rent A Retreat</h1>
+		</div>
+	);
+}
+
+export default App;

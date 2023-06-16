69137dfb30259bd660bb1df26155016082a335e8
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-16 22:32:04 +0530

    [Backend] added crud operations for user host and guest

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,20 +1,210 @@
 import { Request, Response } from 'express';
+import { prisma } from '../db/prisma';
+import generateUser, { generateGuest, generateHost } from '../seedDB/user';
 
-export const getAllUsers = (req: Request, res: Response) => {
-	res.send('get all users controller');
+// todo get all hosts
+// todo get all guests
+// todo error checking
+
+export const getAllUsers = async (req: Request, res: Response) => {
+	try {
+		const users = await prisma.user.findMany();
+		const serializedUsers = users.map((user) => ({
+			...user,
+			phoneNumber: user.phoneNumber.toString(),
+		}));
+
+		res.status(200).json(serializedUsers);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to get all users' });
+	}
 };
 
-export const getUser = (req: Request, res: Response) => {
-	const { userId } = req.params;
-	res.send(`getting user: ${userId}`);
+export const getUser = async (req: Request, res: Response) => {
+	try {
+		const { userId } = req.params;
+		const user = await prisma.user.findFirst({
+			where: {
+				userId,
+			},
+		});
+
+		if (!user) return res.status(404).json({ error: 'Failed to get user' });
+
+		const serializedUser = {
+			...user,
+			phoneNumber: user.phoneNumber.toString(),
+		};
+
+		res.status(200).json(serializedUser);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to get user' });
+	}
 };
 
-export const postUser = (req: Request, res: Response) => {
-	const { name } = req.body;
-	res.send(`posting user with name: ${name}`);
+export const postUser = async (req: Request, res: Response) => {
+	try {
+		const { firstName, lastName, gender, email, phoneNumber, dateOfBirth } =
+			generateUser();
+
+		const user = await prisma.user.create({
+			data: {
+				firstName,
+				lastName,
+				gender,
+				email,
+				phoneNumber,
+				dateOfBirth,
+			},
+		});
+
+		const serializedUser = {
+			...user,
+			phoneNumber: user.phoneNumber.toString(),
+		};
+		res.status(200).json(serializedUser);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to create host' });
+	}
 };
 
-export const deleteUser = (req: Request, res: Response) => {
-	const { userId } = req.params;
-	res.send(`deleting user: ${userId}`);
+export const postHost = async (req: Request, res: Response) => {
+	try {
+		const { userId } = req.body;
+		const { avgRating, totalRatingCount } = generateHost();
+
+		const host = await prisma.host.create({
+			data: {
+				avgRating,
+				totalRatingCount,
+				user: {
+					connect: {
+						userId: userId,
+					},
+				},
+			},
+			include: {
+				user: true,
+			},
+		});
+
+		const serializedHost = {
+			...host,
+			user: {
+				...host.user,
+				phoneNumber: host.user.phoneNumber.toString(),
+			},
+		};
+
+		res.status(200).json(serializedHost);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to create host' });
+	}
+};
+
+export const postGuest = async (req: Request, res: Response) => {
+	try {
+		const { userId } = req.body;
+		const { avgRating, totalRatingCount } = generateGuest();
+
+		const guest = await prisma.guest.create({
+			data: {
+				avgRating,
+				totalRatingCount,
+				user: {
+					connect: {
+						userId: userId,
+					},
+				},
+			},
+			include: {
+				user: true,
+			},
+		});
+
+		const serializedGuest = {
+			...guest,
+			user: {
+				...guest.user,
+				phoneNumber: guest.user.phoneNumber.toString(),
+			},
+		};
+
+		res.status(200).json(serializedGuest);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to create guest' });
+	}
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+	try {
+		const { userId } = req.params;
+
+		const user = await prisma.$transaction([
+			prisma.host.deleteMany({
+				where: {
+					userId,
+				},
+			}),
+			prisma.guest.deleteMany({
+				where: {
+					userId,
+				},
+			}),
+			prisma.user.delete({
+				where: {
+					userId,
+				},
+			}),
+		]);
+
+		console.log(user);
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to delete user' });
+	}
+};
+
+export const deleteHost = async (req: Request, res: Response) => {
+	try {
+		const { hostId } = req.params;
+
+		const host = await prisma.$transaction([
+			prisma.host.delete({
+				where: {
+					hostId,
+				},
+			}),
+		]);
+
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to delete user' });
+	}
+};
+
+export const deleteGuest = async (req: Request, res: Response) => {
+	try {
+		const { guestId } = req.params;
+
+		const guest = await prisma.$transaction([
+			prisma.guest.delete({
+				where: {
+					guestId,
+				},
+			}),
+		]);
+
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Failed to delete user' });
+	}
 };
--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,20 +1,27 @@
 import { Router } from 'express';
 import { prisma } from '../db/prisma';
 import {
+	deleteGuest,
+	deleteHost,
 	deleteUser,
 	getAllUsers,
 	getUser,
+	postGuest,
+	postHost,
 	postUser,
 } from '../controllers/UserController';
 
 const router = Router();
 
 router.get('/', getAllUsers);
-
-router.post('/', postUser);
-
 router.get('/:userId', getUser);
 
+router.post('/', postUser);
+router.post('/host', postHost);
+router.post('/guest', postGuest);
+
 router.delete('/:userId', deleteUser);
+router.delete('/host/:hostId', deleteHost);
+router.delete('/guest/:guestId', deleteGuest);
 
 export default router;

6372601d778777e04cd2da41ee9082a82b4302ac
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-19 20:03:53 +0530

    [Frontend] added navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<nav className='mb-5 w-full sticky top-0 z-50 bg-primary border-b border-secondary shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50'>
+			<div className='py-3 w-11/12 m-auto flex justify-between items-center font-semibold'>
+				<div className='font-bold text-2xl'>RAR</div>
+				<ul className='flex justify-between w-1/3'>
+					<li>
+						<NavLink to='/'>Home</NavLink>
+					</li>
+					<li>
+						<NavLink to='/landing'>About Us</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to='/'
+							className='px-3 py-2 rounded-full bg-secondary text-primary font-normal'>
+							Sign In
+						</NavLink>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;

1d4b24175dde0c42fa2fe04dd4e3e8ef06ad5ee5
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-19 20:01:33 +0530

    added color and font to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#E6E6E8',
+				// primary: '#D4D5DA',
+				// secondary: '#C9D9D2',
+				secondary: '#031D44',
+				accent: '#3B6552',
+			},
+		},
+		fontFamily: {
+			sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+		},
 	},
 	plugins: [],
 };

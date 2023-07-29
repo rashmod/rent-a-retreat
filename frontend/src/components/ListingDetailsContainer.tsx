c60d82b837c95e456e402097007e9dc8e6303147
Bhushan Alhat <alhatbhushan@gmail.com>
2023-07-29 19:59:18 +0530

    [Frontend] styled details page to match with design

--- frontend/src/components/ListingDetailsContainer.tsx
+++ frontend/src/components/ListingDetailsContainer.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { IListingDetail } from '../types/type';
+import { ChevronsDown } from 'lucide-react';
 
 const ListingDetailsContainer = ({
 	user,
@@ -11,41 +12,68 @@ const ListingDetailsContainer = ({
 	houseRule,
 }: IListingDetail) => {
 	return (
-		<div className='col-span-3'>
-			<div className='flex text-2xl'>
-				Hosted by
-				<Link to='/' className='ml-1'>
-					{user.firstName} {user.lastName}
-				</Link>
-			</div>
-			<div className='flex items-center gap-1.5'>
-				<div>{bedroomCount} bedrooms</div>
-				<span className='text-lg leading-none'>&bull;</span>
-				<div>{bathroomCount} bathrooms</div>
-				<span className='text-lg leading-none'>&bull;</span>
-				<div>{maxGuest} guests</div>
-			</div>
-			<div className='grid grid-cols-3'>
-				<div>
-					<span className='font-medium'>Amenities</span>
-					{amenity.map((item) => (
-						<div key={item.amenityId}>{item.amenityTitle}</div>
-					))}
+		<>
+			<div className='col-span-12 text-sm'>
+				<div className='flex text-3xl font-medium'>
+					Hosted by
+					<Link to='/' className='ml-1'>
+						{user.firstName} {user.lastName}
+					</Link>
 				</div>
-				<div>
-					<span className='font-medium'>Categories</span>
-					{category.map((item) => (
-						<div key={item.categoryId}>{item.categoryTitle}</div>
-					))}
-				</div>
-				<div>
-					<span className='font-medium'>House Rules</span>
-					{houseRule.map((item) => (
-						<div key={item.houseRuleId}>{item.ruleTitle}</div>
-					))}
+				<div className='flex items-center gap-2.5'>
+					<div>{bedroomCount} bedrooms</div>
+					<span className='text-lg leading-none'>&bull;</span>
+					<div>{bathroomCount} bathrooms</div>
+					<span className='text-lg leading-none'>&bull;</span>
+					<div>{maxGuest} guests</div>
 				</div>
 			</div>
-		</div>
+
+			<div className='col-span-5 text-sm'>
+				<div className='mt-5'>
+					<span className='text-lg font-medium'>Amenities</span>
+					<ul className='grid grid-cols-4 gap-x-5'>
+						{amenity.slice(0, 8).map((item) => (
+							<li key={item.amenityId} className='truncate'>
+								{item.amenityTitle}
+							</li>
+						))}
+					</ul>
+					<div className='flex items-center justify-center mt-3 text-xs text-center transition cursor-pointer opacity-70 hover:opacity-100 hover:scale-110'>
+						<ChevronsDown height={16} />
+						<span>Know more</span>
+					</div>
+				</div>
+				<div className='mt-5'>
+					<span className='text-lg font-medium'>Categories</span>
+					<ul className='grid grid-cols-4 gap-x-5'>
+						{category.slice(0, 8).map((item) => (
+							<li key={item.categoryId} className='truncate'>
+								{item.categoryTitle}
+							</li>
+						))}
+					</ul>
+					<div className='flex items-center justify-center mt-3 text-xs text-center transition cursor-pointer opacity-70 hover:opacity-100 hover:scale-110'>
+						<ChevronsDown height={16} />
+						<span>Know more</span>
+					</div>
+				</div>
+				<div className='mt-5'>
+					<span className='text-lg font-medium'>House Rules</span>
+					<ul className='grid grid-cols-4 gap-x-5'>
+						{houseRule.slice(0, 8).map((item) => (
+							<li key={item.houseRuleId} className='truncate'>
+								{item.ruleTitle}
+							</li>
+						))}
+					</ul>
+					<div className='flex items-center justify-center mt-3 text-xs text-center transition cursor-pointer opacity-70 hover:opacity-100 hover:scale-110'>
+						<ChevronsDown height={16} />
+						<span>Know more</span>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- frontend/src/components/ListingHeadingContainer.tsx
+++ frontend/src/components/ListingHeadingContainer.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { IListingHeading } from '../types/type';
+import { Edit, Trash2 } from 'lucide-react';
 
 const ListingHeadingContainer = ({
 	listingName,
@@ -9,23 +10,23 @@ const ListingHeadingContainer = ({
 	listingId,
 }: IListingHeading) => {
 	return (
-		<>
-			<div className='flex items-center justify-between font-medium mt-5 mb-0.5'>
-				<span className='text-4xl'>{listingName}</span>
-				<div className='flex gap-4'>
-					<button className='rounded-lg bg-my-secondary-200 px-3 py-1.5 hover:bg-my-secondary-700 hover:text-my-primary-500 transition'>
+		<div className='mb-4'>
+			<div className='flex items-center justify-between font-medium mb-0.5 relative'>
+				<span className='text-5xl'>{listingName}</span>
+				<div className='absolute top-0 right-0 flex gap-4'>
+					<button className='rounded-lg bg-my-primary-900 px-3 py-1.5 hover:bg-my-secondary-I-500 text-white transition'>
 						<Link to={`/listing/${listingId}/edit`}>
-							Edit Listing
+							<Edit />
 						</Link>
 					</button>
-					<button className='rounded-lg bg-my-secondary-200 px-3 py-1.5 hover:bg-my-secondary-700 hover:text-my-primary-500 transition'>
-						Delete Listing
+					<button className='rounded-lg bg-my-primary-900 px-3 py-1.5 hover:bg-red-500 text-white transition duration-200'>
+						<Trash2 />
 					</button>
 				</div>
 			</div>
-			<div className='flex items-end gap-5 text-sm mb-3'>
+			<div className='flex items-end gap-8 mb-3 text-lg'>
 				<div className=''>
-					<span className='text-lg mr-1 leading-none text-orange-400'>
+					<span className='mr-1 text-xl leading-none text-orange-400'>
 						&#9733;
 					</span>
 					{avgRating}
@@ -33,16 +34,12 @@ const ListingHeadingContainer = ({
 					{totalRatingCount} Reviews
 				</div>
 				<div className='flex gap-1 h-max'>
-					{/* <div>unitNumber {address.unitNumber}</div> */}
-					{/* <div>streetName {address.streetName}</div> */}
-					{/* <div>addressLine {address.addressLine}</div> */}
 					<div>{address.city},</div>
-					{/* <div>postalCode {address.postalCode}</div> */}
 					<div>{address.state},</div>
 					<div>{address.country},</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
